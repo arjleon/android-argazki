7f37e3d0c052dfd6e47c45e53262acafd6329546
Armando Leon <ajlelorriaga@gmail.com>
2014-12-13 19:38:35 -0500

    Changed access to public for listener to be provided over the callback() method in the request

--- src/elor/leon/android/argazki/ArgazkiListener.java
+++ src/elor/leon/android/argazki/ArgazkiListener.java
@@ -2,6 +2,6 @@ package elor.leon.android.argazki;
 
 import android.graphics.Bitmap;
 
-interface ArgazkiListener {
+public interface ArgazkiListener {
 	void onDownload(final boolean ok, final ArgazkiRequest request, final Bitmap bitmap);
 }
